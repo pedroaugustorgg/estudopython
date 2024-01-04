8dcab1eeeac2a78657b6303697117f087e95267d
Pedro Guedes <pedroaugustorgg@gmail.com>
2024-01-04 19:40:16 -0300

    Adição do "#" na linha onde o "-*- coding: utf-8" é printado
    A intenção foi evidenciar que tal caractere é essencial, principalmente para quem nunca utilizou

--- Comando_Print.py
+++ Comando_Print.py
@@ -5,7 +5,7 @@ print ("Uma linha de código dessa forma: print (\"Hello World / Olá Mundo.\") 
 print ("Hello World / Olá Mundo.\n") 
 #Observação
 print ("*Os acentos aparecem devido ao código abaixo (que deve ser colocado na primeira linha do código em questão):")
-print ("-*- coding: utf-8 -*-\n")
+print ("\# -*- coding: utf-8 -*-\\n")
 #Comandos utilitários para o Print
 print ("Também existem alguns comandos que podem ser utilizados para ajudarem na elaboração dos 'prints' que você quer que apareça, são eles:\n")
 print ("\\ (barra invertida) serve para inserir um caractere sem interferir no comando, como aspas dentro de um comando \"print\" por exemplo."); print ("\\n serve para você pular uma linha")
