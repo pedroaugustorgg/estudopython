89f824552e691ca08ce74377936c346c824aab31
Pedro Guedes <pedroaugustorgg@gmail.com>
2024-02-02 18:16:56 -0300

    Adição da função REDUCE e ZIP (conteúdo avançado) sobre funções) no arquivo "Funcoes.py"

--- Funcoes.py
+++ Funcoes.py
@@ -44,12 +44,44 @@ valor_dobrado = map(dobro, valor)
 valor_dobrado = list(valor_dobrado)
 
 #Teste3
-print ("\nCompilação interna executada (sem função map):\n")
-print ("Sintaxe:\nprint(dobro(semlista))\n\nResultado:")
+print ("\nCompilação interna executada (sem função map):")
+print ("|Sintaxe|\nprint(dobro(semlista))\n\nResultado:")
 print(dobro(semlista))
 
 #Teste4
-print ("\nCompilação interna executada (utilizando map + lista) 2:\n")
-print ("Sintaxe:\nvalor_dobrado = list(valor_dobrado)\nprint(valor_dobrado)\n\nResultado:")
+print ("\nCompilação interna executada (utilizando map + lista) 2:")
+print ("|Sintaxe|\nvalor_dobrado = list(valor_dobrado)\nprint(valor_dobrado)\n\nResultado:")
 print(valor_dobrado)
+print ("\n---x---\n")
+
+#Python Avançado: Função reduce
+from functools import reduce
+print ("Função REDUCE (conhecimento básico sobre listas recomendado)")
+print ("Definição: É uma função que recebe uma lista e retorna um único valor para esta lista. Segue um exemplo de sintaxe:\n")
+print ("def soma(x, y):\n\treturn x+y\n\nlista_reduce = [1, 3, 5, 10, 20]\nsoma = reduce(soma, lista_reduce)\nprint(soma)")
+
+#Teste5
+print ("\nCompilação interna executada (utilizando reduce + lista):")
+def soma(x, y):
+        return x+y
+
+lista_reduce = [1, 3, 5, 10, 20]
+soma = reduce(soma, lista_reduce)
+print(soma)
+print ("\n---x---\n")
+
+#Python Avançado: Função zip
+print ("Função ZIP (conhecimento básico sobre listas recomendado)")
+print ("Definição: Função utilizada para compactar/concatenar duas ou mais listas, possibilitando percorrê-las utilizando um laço FOR e até mesmo imprimí-la. Segue um exemplo de sintaxe:\n")
+print ("lista1 = [1, 2, 3, 4, 5]\nlista2 = [\"abacate\", \"bola\", \"cachorro\", \"dinheiro\", \"elefante\"]\nlista3 = [\"R$ 2,00\", \"R$ 5,00\", \"Não tem preço\", \"Compra Cachorro\", \"Imensurável\"]\n\nfor numero, palavra, preco in zip(lista1, lista2, lista3):\n\tprint(numero, palavra, \" = \", preco)")
+
+#Teste6
+print ("\nCompilação interna executada (utilizando zip + lista):")
+lista1 = [1, 2, 3, 4, 5]
+lista2 = ["abacate", "bola", "cachorro", "dinheiro", "elefante"]
+lista3 = ["R$ 2,00", "R$ 5,00", "Não tem preço", "Compra Cachorro", "Imensurável"]
+
+for numero, palavra, preco in zip(lista1, lista2, lista3):
+        print(numero, palavra, " = ", preco)
+
 print ("\n---x---\n")
